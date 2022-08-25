7404a71326c13552be114fb2586c61349a473ff4
Jared McMahan <jaredm788@gmail.com>
2022-08-25 11:34:31 -0600

    commit tree 0f0ea52e5317a591299b9fad3f28ed7f603001f9

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// first try
+// function totalBatteries(batteryBatches) {
+//     let batteryCount = []
+//     for(const count of batteryBatches) {
+//         batteryCount += count
+//     }
+//     return batteryCount
+// }
+// console.log(batteryBatches.reduce(totalBatteries,0))
+
+
+// second try 
+// const totalBatteries = (accum, number) => {
+//     let total = accum + number
+//     accum += total
+
+// }
+
+// let total = batteryBatches.reduce(totalBatteries,0)
+
+
+
+// Winner 
+const totalBatteries = batteryBatches.reduce(function(battNumber, totalBatch){
+    return battNumber + totalBatch
+})
+
+// function reducer(totalBatch, batchNumber) {
+//     debugger
+//     return totalBatch +batchNumber
+
+// }
+
+// batteryBatches.reduce(reducer)
